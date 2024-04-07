c3a418367c1aca342c2bd1e7f82051bd28797eef
maksgg <maks.zhovmir.97@gmail.com>
2024-04-07 14:46:06 +0300

    feat: finish basic part of projsect add functional

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ navBtn.onclick = function () {
   nav.classList.toggle('header__top-row--mobile');
   document.body.classList.toggle('no-scroll');
 }
+
+/* Phone Mask */
+
+mask('[data-tel-input]');
+
+/* Delete '+' if nothing input for show placeholder */
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    if(input.value == '+') input.value = '';
+  })
+  input.addEventListener('blur', () => {
+    if(input.value == '+') input.value = '';
+  })
+});
